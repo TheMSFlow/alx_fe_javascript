9a80334dff57a3d9d37d12beac4ed88210419d3f
Michael <themichaelsteve@gmail.com>
2024-07-28 22:54:58 +0100

    added files

---
+++ dom-manipulation/script.js
@@ -0,0 +1,28 @@
+const quoteDisplay = document.getElementById('quoteDisplay');
+const quoteCategory = document.getElementById('quoteCategory');
+const newQuote = document.getElementById('newQuote');
+
+const quotes = [
+    {text: "Do hard things" , category: "Education"},
+    {text: "Black and Yellow" , category: "Music"},
+    {text: "I have a dream" , category: "Inspirational"},
+    {text: "Is there no one else?" , category: "Movie"},
+    {text: "God is the greatest!" , category: "Religion"},
+    {text: "What's the cost of one customer?" , category: "Business"}
+]
+
+// console.log(quotes[0].text);
+
+function showRandomQuote (){
+    const randomQuote = Math.floor(Math.random() * quotes.length)
+    return quotes[randomQuote].text;
+}
+
+// console.log(showRandomQuote());
+
+function createAddQuoteForm () {
+    quotes.push({text: "Beema, Benz, Bentley" , category: "Cars"});
+}
+
+console.log(createAddQuoteForm());
+console.log(quotes);
